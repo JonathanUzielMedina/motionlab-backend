ee14bbc2ea92392b43200c6e8bc5ef6323166c56
Crlss123 <camadortjr@gmail.com>
2025-04-22 15:36:28 -0700

    Estructura general de proyecto

---
+++ index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+import connectionDB from "./src/connection/connection";
+const morgan = require("morgan");
+const cors = require("cors");
+const app = express();
+const PORT = 3000;
+
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
+
+connectionDB();
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
