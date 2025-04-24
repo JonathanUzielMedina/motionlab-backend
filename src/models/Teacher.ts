0fa7954144e5be54e9ced1c9ed267c2a8d7e36e6
Crlss123 <camadortjr@gmail.com>
2025-04-23 20:31:32 -0700

    Crea modelo Teacher sin relaciones

--- src/models/Teacher.ts
+++ src/models/Teacher.ts
@@ -1,7 +1,32 @@
-import { Table, Model, Column, HasMany } from "sequelize-typescript";
+import {
+  Table,
+  Model,
+  Column,
+  HasMany,
+  PrimaryKey,
+  DataType,
+} from "sequelize-typescript";
 import { Optional } from "sequelize";
+import { table } from "console";
 
 interface TeacherAttributes {
   id: string;
   pwd: string;
+}
+
+interface TeacherCreationAttributes extends Optional<TeacherAttributes, "id"> {}
+
+@Table({
+  tableName: "teachers",
+})
+export class Teacher extends Model<
+  TeacherAttributes,
+  TeacherCreationAttributes
+> {
+  @PrimaryKey
+  @Column(DataType.STRING)
+  declare id: string;
+
+  @Column(DataType.STRING)
+  pwd!: string;
 }
