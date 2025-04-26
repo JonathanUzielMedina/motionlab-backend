2c5126cb83a6b2acc739d8f947308126a6998b10
Marcoibarra898 <A01253370@tec.mx>
2025-04-26 14:57:24 -0700

    crea endpoint para obtener la informacion de las rondas junto con los scores de los usuarios y de los equipos

--- src/controllers/StudentTeamController.ts
+++ src/controllers/StudentTeamController.ts
@@ -2,42 +2,41 @@ import { Request, Response, RequestHandler } from "express";
 import { StudentTeam } from "../models/StudentTeam";
 import { Student } from "../models/Student";
 import { Team } from "../models/Team";
-
 export const getStudentTeamById: RequestHandler = async (
-  req: Request,
-  res: Response
+    req: Request,
+    res: Response
 ) => {
-  try {
-    const { id } = req.params;
-    const rawData: StudentTeam[] = await StudentTeam.findAll({
-      where: {
-        id_team: id,
-      },
-      include: [
-        {
-          model: Student,
-        },
-        {
-          model: Team,
-        }
-      ],
-    });
-    
-    const data = rawData.map((element) => ({
-      studentId: element.student.id,
-      teamId: element.team.id,
-    }));
-    
-    res.status(200).json({
-      message: "Estudiantes del equipo obtenidos exitosamente",
-      payload: data,
-      status: "success",
-    });
-  } catch (error) {
-    res.status(500).json({
-      message: "Error en el servidor",
-      payload: null,
-      status: "error",
-    });
-  }
+    try {
+        const { id } = req.params;
+        const rawData: StudentTeam[] = await StudentTeam.findAll({
+            where: {
+                id_team: id,
+            },
+            include: [
+                {
+                    model: Student,
+                },
+                {
+                    model: Team,
+                }
+            ],
+        });
+        
+        const data = rawData.map((element) => ({
+            studentId: element.student.id,
+            teamId: element.team.id,
+        }));
+        
+        res.status(200).json({
+            message: "Estudiantes del equipo obtenidos exitosamente",
+            payload: data,
+            status: "success",
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "Error en el servidor",
+            payload: null,
+            status: "error",
+        });
+    }
 };
